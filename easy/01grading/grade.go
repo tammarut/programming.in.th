b5abe8ed7637d0043f81a588f42f9e5b3816060c
tammarut <tammarut.nua@gmail.com>
2019-04-08 20:25:06 +0700

    01grading: split func scanPoint; only 2 step in main

--- easy/01grading/grade.go
+++ easy/01grading/grade.go
@@ -5,25 +5,28 @@ import "fmt"
 func main() {
 	var a, b, c int
 
+	scanPoint(&a, &b, &c)
+	fmt.Println(getGrade(a + b + c))
+
+}
+func scanPoint(a, b, c *int) {
 	fmt.Print("คะแนนเก็บ(0-30): ")
-	fmt.Scanln(&a)
-	if a >= 0 && a <= 30 {
-		fmt.Print("คะแนนกลางภาค(0-30): ")
-		fmt.Scanln(&b)
-		if b >= 0 && b <= 30 {
-			fmt.Print("คะแนนปลายภาค(0-40): ")
-			fmt.Scanln(&c)
-			if (c < 0) || (c > 40) {
-				fmt.Println("Please key 0-40")
-			}
-		} else {
-			fmt.Println("Please key 0-30!")
-		}
-	} else {
-		fmt.Println("Please key 0-30!")
+	fmt.Scanln(a)
+	if *a < 0 || *a > 30 {
+		fmt.Println("Please key 0-30")
 	}
 
-	fmt.Println(getGrade(a + b + c))
+	fmt.Print("คะแนนกลางภาค(0-30): ")
+	fmt.Scanln(b)
+	if *b < 0 || *b > 30 {
+		fmt.Println("Please key 0-30")
+	}
+
+	fmt.Print("คะแนนปลายภาค(0-40): ")
+	fmt.Scanln(c)
+	if *c < 0 || *c > 40 {
+		fmt.Println("Please key 0-40")
+	}
 
 }
 
