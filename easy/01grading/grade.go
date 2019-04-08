39894cbf415543c7c237d99215313df9a6427b20
tammarut <tammarut.nua@gmail.com>
2019-04-08 17:52:01 +0700

    scan c(คะแนนปลายภาค) by 0-40

--- easy/01grading/grade.go
+++ easy/01grading/grade.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
+	var a, b, c int
 
 	fmt.Print("คะแนนเก็บ: ")
 	fmt.Scanln(&a)
@@ -11,7 +11,11 @@ func main() {
 		fmt.Print("คะแนนกลางภาค: ")
 		fmt.Scanln(&b)
 		if b >= 0 && b <= 30 {
-			fmt.Print(b)
+			fmt.Print("คะแนนปลายภาค: ")
+			fmt.Scanln(&c)
+			if (c < 0) || (c > 40) {
+				fmt.Println("Please key 0-40")
+			}
 		} else {
 			fmt.Println("Please key 0-30!")
 		}
