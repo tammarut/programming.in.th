f00854a0dd84ee27ed5033033c9c5b9654c2e40b
tammarut <tammarut.nua@gmail.com>
2019-04-09 15:39:18 +0700

    01grading: unit-test(B C D F)

--- easy/01grading/grade_test.go
+++ easy/01grading/grade_test.go
@@ -2,9 +2,55 @@ package main
 
 import "testing"
 
-func TestGetGradeWhenPassSumShouldReturnString(t *testing.T) {
+func TestGetGradeWhenPassSum80ShouldReturnA(t *testing.T) {
 	result := getGrade(80)
 	if result != "A" {
 		t.Error("Expected: A", "but Got->", result)
+	}
+}
+func TestGetGradeWhenPassSum75ShouldReturnBplus(t *testing.T) {
+	result := getGrade(75)
+	if result != "B+" {
+		t.Error("Expected: B+", "but Got->", result)
+	}
+}
+func TestGetGradeWhenPassSum70ShouldReturnB(t *testing.T) {
+	result := getGrade(70)
+	if result != "B" {
+		t.Error("Expected: B", "but Got->", result)
+	}
+}
+func TestGetGradeWhenPassSum65ShouldReturnCplus(t *testing.T) {
+	result := getGrade(65)
+	if result != "C+" {
+		t.Error("Expected: C+", "but Got->", result)
+	}
+}
+
+func TestGetGradeWhenPassSum60ShouldReturnC(t *testing.T) {
+	result := getGrade(60)
+	if result != "C" {
+		t.Error("Expected: C", "but Got->", result)
+	}
+}
+
+func TestGetGradeWhenPassSum55ShouldReturnDplus(t *testing.T) {
+	result := getGrade(55)
+	if result != "D+" {
+		t.Error("Expected: D+", "but Got->", result)
+	}
+}
+
+func TestGetGradeWhenPassSum50ShouldReturnD(t *testing.T) {
+	result := getGrade(50)
+	if result != "D" {
+		t.Error("Expected: D", "but Got->", result)
+	}
+}
+
+func TestGetGradeWhenPassSum49ShouldReturnF(t *testing.T) {
+	result := getGrade(49)
+	if result != "F" {
+		t.Error("Expected: D", "but Got->", result)
 	}
 }
