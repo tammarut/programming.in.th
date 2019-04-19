2939eca839cb2937b7c038bec907ae5dc5101bc2
tammarut <tammarut>
2019-04-19 15:23:22 +0700

    02min-max: comment explain

--- easy/02min-max/min-max.go
+++ easy/02min-max/min-max.go
@@ -1,5 +1,6 @@
 package main
 
+//! หาMinimum-Maximum
 import (
 	"fmt"
 	"log"
@@ -7,17 +8,20 @@ import (
 )
 
 func main() {
+	//=> 1) Enter size of box
 	var size int
-
 	fmt.Print("Enter size of box: ")
 	fmt.Scanln(&size)
 
+	//=> 2) Create slice depend on size
 	box := make([]int, size)
 
+	//=> 3) Enter numbers
 	fmt.Printf("Enter %d numbers\n", size)
 	for i := 0; i < len(box); i++ {
 		fmt.Printf("\t %d)", i+1)
 
+		//! handle: ถ้ามากกว่า1 หรือน้อยกว่า1000 ไปต่อ
 		for {
 			fmt.Scanln(&box[i])
 			if box[i] >= 1 && box[i] <= 1000 {
@@ -27,7 +31,10 @@ func main() {
 		}
 	}
 
+	//=> 4) Sort numbers| Ascending
 	sort.Ints(box)
+
+	//=> 5) output Min/Max
 	fmt.Printf("Min: %d\n", box[0])
 	fmt.Printf("Max: %d\n", box[len(box)-1])
 }
